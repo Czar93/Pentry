a595c30442389a62ea87a702206bed089387f406
Adam Czarnecki <aczarnecki93@gmail.com>
2020-10-01 14:15:38 +0200

    Updates

---
+++ precache-manifest.6e0cfa9ed4765aa3350a89160e528b78.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122aeaa3e033a503ad0daaff2990cb17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b1ab3abe3c2d39f6650",
+    "url": "/static/js/2.8ab18a90.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8ab18a90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201e05d20f67f18dc7e9",
+    "url": "/static/js/main.7d87aabb.chunk.js"
+  },
+  {
+    "revision": "8d3b77b27f19cdb0ebde",
+    "url": "/static/js/runtime-main.7f8b1ec2.js"
+  },
+  {
+    "revision": "b49a4dcb403831afada00ed86de36a3d",
+    "url": "/static/media/buy.b49a4dcb.svg"
+  },
+  {
+    "revision": "58290411c16213407c2445cd6aad050c",
+    "url": "/static/media/cancel.58290411.svg"
+  },
+  {
+    "revision": "9321562d36f30728ad6de0faaad67049",
+    "url": "/static/media/logo.9321562d.svg"
+  },
+  {
+    "revision": "27b127d30bccd3bb2efd092d4309e9f6",
+    "url": "/static/media/pen.27b127d3.svg"
+  },
+  {
+    "revision": "a0ff3fda12aaf124d563652390bbc174",
+    "url": "/static/media/plus.a0ff3fda.svg"
+  },
+  {
+    "revision": "9414d93b1f76e4330b446125d5c2b68e",
+    "url": "/static/media/search.9414d93b.svg"
+  },
+  {
+    "revision": "ee6069c5e5e47926feef5f638c7be05c",
+    "url": "/static/media/wallpaper.ee6069c5.jpg"
+  }
+]);
